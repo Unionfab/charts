504e574f2076638714407bcc265200979174605e
lotuc <lotu.c@outlook.com>
2020-10-16 16:48:08 +0800

    chore: package.sh

--- package.sh
+++ package.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd $(dirname ${0})
-mkdir -p docs
-cd docs
+
+mkdir -p docs && cd docs
 
 for c in $(ls ../ufc); do
   echo "packing ufc/${c}"
@@ -10,3 +10,7 @@ for c in $(ls ../ufc); do
 done
 
 helm repo index . --url https://unionfab.github.io/charts/
+
+TMP_DIR=$(mktemp -d -t ufc-charts.XXXXXXXXXX)
+cd .. && mv docs/* ${TMP_DIR}
+git checkout gh-pages && rm -rf docs && mv ${TMP_DIR} ./docs
