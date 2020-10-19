1b4c8e90ef8d761a919e0b6be645259c04fc6112
lotuc <lotu.c@outlook.com>
2020-10-19 09:24:52 +0800

    chore: udpate

--- package.sh
+++ package.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+args=("$@")
 cd $(dirname ${0})
 
+TMP_DIR=$(mktemp -d -t ufc-charts)
 mkdir -p docs && cd docs
 
-for c in $(ls ../ufc); do
-  echo "packing ufc/${c}"
-  helm package ../ufc/${c}
+for ((i = 0; i < $#; i++)); do
+  echo "packing: ufc/${args[$i]}"
+  helm package "../ufc/${args[$i]}"
 done
 
-helm repo index . --url https://unionfab.github.io/charts/
+cd ..
+for i in $(ls "${TMP_DIR}"); do
+  mv "docs/${i}" "${TMP_DIR}/"
+done
 
-TMP_DIR=$(mktemp -d -t ufc-charts.XXXXXXXXXX)
-cd .. && mv docs/* ${TMP_DIR}
-git checkout gh-pages && rm -rf docs && mv ${TMP_DIR} ./docs
+git checkout gh-pages
+mkdir -p docs
+for i in $(ls "${TMP_DIR}"); do
+  mv "${TMP_DIR}/${i}" ./docs/
+done
+
+echo "indexing https://unionfab.github.io/charts/"
+cd docs && helm repo index . --url https://unionfab.github.io/charts/
